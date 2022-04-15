4b373f690cee784e9915138073b645235b51de9e
Alice Huang <alicehccn@gmail.com>
2022-04-14 19:47:19 -0700

    use react.component for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,51 @@
+import React from 'react'
 import CanvasDraw from 'react-canvas-draw'
 import './App.scss'
 
-const defaultProps = {
-  className: 'canvas',
-  onChange: null,
-  loadTimeOffset: 5,
-  lazyRadius: 10,
-  brushRadius: 1,
-  brushColor: '#eee',
-  catenaryColor: '#aaa',
-  gridColor: 'rgba(150,150,150,0.17)',
-  hideGrid: true
-  // canvasWidth: 400,
-  // canvasHeight: 400,
-  // disabled: false,
-  // imgSrc: "",
-  // saveData: null,
-  // immediateLoading: false,
-  // hideInterface: false,
-  // gridSizeX: 25,
-  // gridSizeY: 25,
-  // gridLineWidth: 0.5,
-  // hideGridX: false,
-  // hideGridY: false,
-  // enablePanAndZoom: false,
-  // mouseZoomFactor: 0.01,
-  // zoomExtents: { min: 0.33, max: 3 },
+
+
+class App extends React.Component {
+  static defaultProps = {
+    className: 'canvas',
+    onChange: null,
+    loadTimeOffset: 5,
+    lazyRadius: 10,
+    brushRadius: 1,
+    brushColor: '#eee',
+    catenaryColor: '#aaa',
+    gridColor: 'rgba(150,150,150,0.17)',
+    hideGrid: true
+    // canvasWidth: 400,
+    // canvasHeight: 400,
+    // disabled: false,
+    // imgSrc: "",
+    // saveData: null,
+    // immediateLoading: false,
+    // hideInterface: false,
+    // gridSizeX: 25,
+    // gridSizeY: 25,
+    // gridLineWidth: 0.5,
+    // hideGridX: false,
+    // hideGridY: false,
+    // enablePanAndZoom: false,
+    // mouseZoomFactor: 0.01,
+    // zoomExtents: { min: 0.33, max: 3 },
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header' />
+        <CanvasDraw
+          {...App.defaultProps}
+        />
+        <textarea className='editor' placeholder='Write something.' />
+      </div>
+    )
+  }
+  
 }
 
-function App (): any {
-  return (
-    <div className='App'>
-      <header className='App-header' />
-      <CanvasDraw
-        {...defaultProps}
-      />
-      <textarea className='editor' placeholder='Write something.' />
-    </div>
-  )
-}
+
 
 export default App
