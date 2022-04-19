e34dd7ae3490b1ec5336662bdf6fd5d2e99b2a37
Alice Huang <alicehccn@gmail.com>
2022-04-18 20:36:28 -0700

    canvas toolbar css

--- src/Draw.tsx
+++ src/Draw.tsx
@@ -1,42 +1,44 @@
 import { useRef, useState } from 'react'
-import { saveAs } from 'file-saver';
 import CanvasDraw from 'react-canvas-draw'
-
-
 import './Draw.scss'
 
 
 export default function Draw () {
   const canvasRef = useRef(null)
+  const urlRef = useRef(null)
   const [dataUrl, saveData] = useState('')
   const draw = () => {
-    console.log(canvasRef)
+    console.log('drawing')
   }
 
   const undo = () => {
+    console.log('undoing')
     canvasRef.current.undo()
   }
 
   const erase = () => {
+    console.log('erasing')
     canvasRef.current.eraseAll()
   }
 
   const save = () => {
+    console.log('saving')
     canvasRef.current.getSaveData()
     const url = canvasRef.current.getDataURL()
-    saveData(url)
+    if (url) {
+      saveData(url)
+    }
   }
 
-  const loadSaved = () => {
-    console.log(dataUrl)
-
-    const file = new Blob(dataUrl, 'image.jpg');
-    saveAs(file)
+  const download = () => {
+    console.log('downloading')
+    urlRef.current.click()
   }
 
   const defaultProps = {
     className: 'canvas',
     ref: canvasRef,
+    backgroundColor: 'transparent',
     loadTimeOffset: 5,
     lazyRadius: 0,
     brushRadius: 0,
@@ -46,8 +48,7 @@ export default function Draw () {
 
   return (
       <div className='canvas-container'>
-        
-        <ul className='canvas-tools'>
+        <ul className='canvas-toolbar'>
           <li className='button'>
             <img src='./assets/icon-pencil.png' onClick={draw}/>
           </li>
@@ -61,14 +62,13 @@ export default function Draw () {
             <img src='./assets/icon-save.png' onClick={save}/>
           </li>
           <li className='button'>
-            <img src='./assets/icon-download.png' onClick={loadSaved}/>
+            <img src='./assets/icon-download.png' onClick={download}/>
           </li>
         </ul>
         <CanvasDraw
           {...defaultProps}
-          onChange={draw}
         />
+        <a ref={urlRef} href={dataUrl} download="drawing.png" />
     </div>
   )
 }
-
