50c08d4f1c412082cdd4392c5a990c6fcc549112
Alice Huang <alicehccn@gmail.com>
2022-04-18 17:26:11 -0700

    drawing tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,13 @@
-import React from 'react'
-import CanvasDraw from 'react-canvas-draw'
 import './App.scss'
+import Draw from './Draw'
 
-class App extends React.Component {
-  static defaultProps = {
-    className: 'canvas',
-    onChange: null,
-    loadTimeOffset: 1,
-    lazyRadius: 0,
-    brushRadius: 0,
-    catenaryColor: '#aaa',
-    gridColor: 'rgba(150,150,150,0.17)',
-    hideGrid: true
-  }
-
-  state = {
-    mode: 'drawing'
-  }
-
-  switchMode (): void {
-    this.setState({ mode: this.state.mode === 'drawing' ? 'typing' : 'drawing' })
-  }
-
-  render (): JSX.Element {
-    return (
-      <div className='App'>
-        <div className='menu'>
-          <input type='button' value={this.state.mode} onClick={this.switchMode.bind(this)} />
-        </div>
-
-        <div className='canvas-container' hidden={this.state.mode === 'typing'}>
-          <CanvasDraw
-            {...App.defaultProps}
-          />
-        </div>
-
-        <div className='editor-container' hidden={this.state.mode === 'drawing'}>
-          <textarea className='editor' placeholder='Write something.' />
-        </div>
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <div className='App'>
+      <Draw />
+      {/* <div className='editor-container'}>
+        <textarea className='editor' placeholder='Write something.' />
+      </div> */}
+    </div>
+  )
 }
-
-export default App
