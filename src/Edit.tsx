80fa81f4b94a33433b0f5a7c1ada4a208025c4d3
Alice Huang <alicehccn@gmail.com>
2022-05-01 20:05:58 -0700

    prototype theme selection

--- src/Edit.tsx
+++ src/Edit.tsx
@@ -1,9 +1,13 @@
 import AceEditor from 'react-ace'
 import ace from 'ace-builds/src-noconflict/ace'
 import './Edit.scss'
+import './Draw.scss'
 
 import './lib/modes'
+import { modelist } from './lib/modes'
+
 import './lib/themes'
+import { themelist } from './lib/themes'
 
 
 export default function Edit (): JSX.Element {
@@ -11,20 +15,33 @@ export default function Edit (): JSX.Element {
   const onChange = (value: string): void => {
     console.log(value)
   }
-
   const defaultProps = {
     onChange: onChange,
     name: 'editor',
     className: 'editor',
     theme: 'monokai',
     mode: 'javascript',
-    fontSize: 16,
+    fontSize: 14,
     wrapEnabled: true,
-    placeholder: 'Type something',
+    placeholder: 'Type something'
+  }
+
+  const switchTheme = (e:any) => {
+    console.log(e.target.value)
   }
 
   return (
     <div className='editor-container'>
+      <ul className='editor-toolbar'>
+        <li className='button'>
+          <select onChange={switchTheme}>
+            {themelist.map((theme) => (
+              <option key={theme.url} value={theme.name}>{theme.name}</option>
+            ))}
+          </select>
+        </li>
+
+      </ul>
       <AceEditor
         {...defaultProps}
       />
---
+++ src/lib/themes.ts
@@ -0,0 +1,83 @@
+
+
+export * from 'brace/theme/ambiance'
+export * from 'brace/theme/eclipse'
+export * from 'brace/theme/merbivore_soft'
+export * from 'brace/theme/tomorrow_night'
+export * from 'brace/theme/chaos'
+export * from 'brace/theme/github'
+export * from 'brace/theme/mono_industrial'
+export * from 'brace/theme/tomorrow_night_blue'
+export * from 'brace/theme/chrome'
+export * from 'brace/theme/gob'
+export * from 'brace/theme/monokai'
+export * from 'brace/theme/tomorrow_night_bright'
+export * from 'brace/theme/clouds'
+export * from 'brace/theme/gruvbox'
+export * from 'brace/theme/pastel_on_dark'
+export * from 'brace/theme/tomorrow_night_eighties'
+export * from 'brace/theme/clouds_midnight'
+export * from 'brace/theme/idle_fingers'
+export * from 'brace/theme/solarized_dark'
+export * from 'brace/theme/twilight'
+export * from 'brace/theme/cobalt'
+export * from 'brace/theme/iplastic'
+export * from 'brace/theme/solarized_light'
+export * from 'brace/theme/vibrant_ink'
+export * from 'brace/theme/crimson_editor'
+export * from 'brace/theme/katzenmilch'
+export * from 'brace/theme/sqlserver'
+export * from 'brace/theme/xcode'
+export * from 'brace/theme/dawn'
+export * from 'brace/theme/kr_theme'
+export * from 'brace/theme/terminal'
+export * from 'brace/theme/dracula'
+export * from 'brace/theme/kuroir'
+export * from 'brace/theme/textmate'
+export * from 'brace/theme/dreamweaver'
+export * from 'brace/theme/merbivore'
+export * from 'brace/theme/tomorrow'
+
+const themelist = [
+  {name: 'ambiance', url: 'brace/theme/ambiance'},
+  {name: 'eclipse', url: 'brace/theme/eclipse'},
+  {name: 'merbivore_soft', url: 'brace/theme/merbivore_soft'},
+  {name: 'tomorrow_night', url: 'brace/theme/tomorrow_night'},
+  {name: 'chaos', url: 'brace/theme/chaos'},
+  {name: 'github', url: 'brace/theme/github'},
+  {name: 'mono_industrial', url: 'brace/theme/mono_industrial'},
+  {name: 'tomorrow_night_blue', url: 'brace/theme/tomorrow_night_blue'},
+  {name: 'chrome', url: 'brace/theme/chrome'},
+  {name: 'gob', url: 'brace/theme/gob'},
+  {name: 'monokai', url: 'brace/theme/monokai'},
+  {name: 'tomorrow_night_bright', url: 'brace/theme/tomorrow_night_bright'},
+  {name: 'clouds', url: 'brace/theme/clouds'},
+  {name: 'gruvbox', url: 'brace/theme/gruvbox'},
+  {name: 'pastel_on_dark', url: 'brace/theme/pastel_on_dark'},
+  {name: 'tomorrow_night_eighties', url: 'brace/theme/tomorrow_night_eighties'},
+  {name: 'clouds_midnight', url: 'brace/theme/clouds_midnight'},
+  {name: 'idle_fingers', url: 'brace/theme/idle_fingers'},
+  {name: 'solarized_dark', url: 'brace/theme/solarized_dark'},
+  {name: 'twilight', url: 'brace/theme/twilight'},
+  {name: 'cobalt', url: 'brace/theme/cobalt'},
+  {name: 'iplastic', url: 'brace/theme/iplastic'},
+  {name: 'solarized_light', url: 'brace/theme/solarized_light'},
+  {name: 'vibrant_ink', url: 'brace/theme/vibrant_ink'},
+  {name: 'crimson_editor', url: 'brace/theme/crimson_editor'},
+  {name: 'katzenmilch', url: 'brace/theme/katzenmilch'},
+  {name: 'sqlserver', url: 'brace/theme/sqlserver'},
+  {name: 'xcode', url: 'brace/theme/xcode'},
+  {name: 'dawn', url: 'brace/theme/dawn'},
+  {name: 'kr_theme', url: 'brace/theme/kr_theme'},
+  {name: 'terminal', url: 'brace/theme/terminal'},
+  {name: 'dracula', url: 'brace/theme/dracula'},
+  {name: 'kuroir', url: 'brace/theme/kuroir'},
+  {name: 'textmate', url: 'brace/theme/textmate'},
+  {name: 'dreamweaver', url: 'brace/theme/dreamweaver'},
+  {name: 'merbivore', url: 'brace/theme/merbivore'},
+  {name: 'tomorrow', url: 'brace/theme/tomorrow}'}
+]
+
+export {
+  themelist
+}
