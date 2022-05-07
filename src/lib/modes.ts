d344e338ce8281f37fd9bddf1e246bab196f3a7f
Alice Huang <alicehccn@gmail.com>
2022-05-06 20:03:01 -0700

    0.1.1-dev (#14)
    * import modes and themes in separate files
    
    * prototype theme selection
    
    * bootstrap mui
    
    * can set theme now
    
    * restructure folder
    
    * rename some stuff
    
    * Canvas redesign (#13)
    
    * simplied canvas design
    
    * modulizing cpm
    
    * same as above
    
    * refactor constainers/components
    
    * clean up
    
    * merge changes to root
    
    * linting
    
    * update version

---
+++ src/lib/modes.ts
@@ -0,0 +1,309 @@
+export * from 'brace/mode/abap'
+export * from 'brace/mode/eiffel'
+export * from 'brace/mode/json'
+export * from 'brace/mode/nsis'
+export * from 'brace/mode/snippets'
+export * from 'brace/mode/abc'
+export * from 'brace/mode/ejs'
+export * from 'brace/mode/jsoniq'
+export * from 'brace/mode/objectivec'
+export * from 'brace/mode/soy_template'
+export * from 'brace/mode/actionscript'
+export * from 'brace/mode/elixir'
+export * from 'brace/mode/jsp'
+export * from 'brace/mode/ocaml'
+export * from 'brace/mode/space'
+export * from 'brace/mode/ada'
+export * from 'brace/mode/elm'
+export * from 'brace/mode/jssm'
+export * from 'brace/mode/pascal'
+export * from 'brace/mode/sparql'
+export * from 'brace/mode/apache_conf'
+export * from 'brace/mode/erlang'
+export * from 'brace/mode/jsx'
+export * from 'brace/mode/perl'
+export * from 'brace/mode/sql'
+export * from 'brace/mode/applescript'
+export * from 'brace/mode/forth'
+export * from 'brace/mode/julia'
+export * from 'brace/mode/pgsql'
+export * from 'brace/mode/sqlserver'
+export * from 'brace/mode/asciidoc'
+export * from 'brace/mode/fortran'
+export * from 'brace/mode/kotlin'
+export * from 'brace/mode/php'
+export * from 'brace/mode/stylus'
+export * from 'brace/mode/assembly_x86'
+export * from 'brace/mode/ftl'
+export * from 'brace/mode/latex'
+export * from 'brace/mode/pig'
+export * from 'brace/mode/svg'
+export * from 'brace/mode/autohotkey'
+export * from 'brace/mode/gcode'
+export * from 'brace/mode/lean'
+export * from 'brace/mode/plain_text'
+export * from 'brace/mode/swift'
+export * from 'brace/mode/batchfile'
+export * from 'brace/mode/gherkin'
+export * from 'brace/mode/less'
+export * from 'brace/mode/powershell'
+export * from 'brace/mode/swig'
+export * from 'brace/mode/bro'
+export * from 'brace/mode/gitignore'
+export * from 'brace/mode/liquid'
+export * from 'brace/mode/praat'
+export * from 'brace/mode/tcl'
+export * from 'brace/mode/c9search'
+export * from 'brace/mode/glsl'
+export * from 'brace/mode/lisp'
+export * from 'brace/mode/prolog'
+export * from 'brace/mode/tex'
+export * from 'brace/mode/c_cpp'
+export * from 'brace/mode/gobstones'
+export * from 'brace/mode/live_script'
+export * from 'brace/mode/properties'
+export * from 'brace/mode/text'
+export * from 'brace/mode/cirru'
+export * from 'brace/mode/golang'
+export * from 'brace/mode/livescript'
+export * from 'brace/mode/protobuf'
+export * from 'brace/mode/textile'
+export * from 'brace/mode/clojure'
+export * from 'brace/mode/graphqlschema'
+export * from 'brace/mode/logiql'
+export * from 'brace/mode/python'
+export * from 'brace/mode/toml'
+export * from 'brace/mode/cobol'
+export * from 'brace/mode/groovy'
+export * from 'brace/mode/lsl'
+export * from 'brace/mode/r'
+export * from 'brace/mode/tsx'
+export * from 'brace/mode/haml'
+export * from 'brace/mode/lua'
+export * from 'brace/mode/razor'
+export * from 'brace/mode/turtle'
+export * from 'brace/mode/coldfusion'
+export * from 'brace/mode/handlebars'
+export * from 'brace/mode/luapage'
+export * from 'brace/mode/rdoc'
+export * from 'brace/mode/twig'
+export * from 'brace/mode/csharp'
+export * from 'brace/mode/haskell'
+export * from 'brace/mode/lucene'
+export * from 'brace/mode/red'
+export * from 'brace/mode/typescript'
+export * from 'brace/mode/csound_document'
+export * from 'brace/mode/haskell_cabal'
+export * from 'brace/mode/makefile'
+export * from 'brace/mode/rhtml'
+export * from 'brace/mode/vala'
+export * from 'brace/mode/csound_orchestra'
+export * from 'brace/mode/haxe'
+export * from 'brace/mode/markdown'
+export * from 'brace/mode/rst'
+export * from 'brace/mode/vbscript'
+export * from 'brace/mode/csound_score'
+export * from 'brace/mode/hjson'
+export * from 'brace/mode/mask'
+export * from 'brace/mode/ruby'
+export * from 'brace/mode/velocity'
+export * from 'brace/mode/css'
+export * from 'brace/mode/html'
+export * from 'brace/mode/matlab'
+export * from 'brace/mode/rust'
+export * from 'brace/mode/verilog'
+export * from 'brace/mode/curly'
+export * from 'brace/mode/html_elixir'
+export * from 'brace/mode/mavens_mate_log'
+export * from 'brace/mode/sass'
+export * from 'brace/mode/vhdl'
+export * from 'brace/mode/d'
+export * from 'brace/mode/html_ruby'
+export * from 'brace/mode/maze'
+export * from 'brace/mode/scad'
+export * from 'brace/mode/wollok'
+export * from 'brace/mode/dart'
+export * from 'brace/mode/ini'
+export * from 'brace/mode/mel'
+export * from 'brace/mode/scala'
+export * from 'brace/mode/xml'
+export * from 'brace/mode/diff'
+export * from 'brace/mode/io'
+export * from 'brace/mode/mips_assembler'
+export * from 'brace/mode/scheme'
+export * from 'brace/mode/xquery'
+export * from 'brace/mode/django'
+export * from 'brace/mode/jack'
+export * from 'brace/mode/mipsassembler'
+export * from 'brace/mode/scss'
+export * from 'brace/mode/yaml'
+export * from 'brace/mode/dockerfile'
+export * from 'brace/mode/jade'
+export * from 'brace/mode/mushcode'
+export * from 'brace/mode/sh'
+export * from 'brace/mode/dot'
+export * from 'brace/mode/java'
+export * from 'brace/mode/mysql'
+export * from 'brace/mode/sjs'
+export * from 'brace/mode/drools'
+export * from 'brace/mode/javascript'
+export * from 'brace/mode/nix'
+export * from 'brace/mode/smarty'
+
+const modelist = [
+  { name: 'abap', path: 'brace/mode/abap' },
+  { name: 'eiffel', path: 'brace/mode/eiffel' },
+  { name: 'json', path: 'brace/mode/json' },
+  { name: 'nsis', path: 'brace/mode/nsis' },
+  { name: 'snippets', path: 'brace/mode/snippets' },
+  { name: 'abc', path: 'brace/mode/abc' },
+  { name: 'ejs', path: 'brace/mode/ejs' },
+  { name: 'jsoniq', path: 'brace/mode/jsoniq' },
+  { name: 'objectivec', path: 'brace/mode/objectivec' },
+  { name: 'soy_template', path: 'brace/mode/soy_template' },
+  { name: 'actionscript', path: 'brace/mode/actionscript' },
+  { name: 'elixir', path: 'brace/mode/elixir' },
+  { name: 'jsp', path: 'brace/mode/jsp' },
+  { name: 'ocaml', path: 'brace/mode/ocaml' },
+  { name: 'space', path: 'brace/mode/space' },
+  { name: 'ada', path: 'brace/mode/ada' },
+  { name: 'elm', path: 'brace/mode/elm' },
+  { name: 'jssm', path: 'brace/mode/jssm' },
+  { name: 'pascal', path: 'brace/mode/pascal' },
+  { name: 'sparql', path: 'brace/mode/sparql' },
+  { name: 'apache_conf', path: 'brace/mode/apache_conf' },
+  { name: 'erlang', path: 'brace/mode/erlang' },
+  { name: 'jsx', path: 'brace/mode/jsx' },
+  { name: 'perl', path: 'brace/mode/perl' },
+  { name: 'sql', path: 'brace/mode/sql' },
+  { name: 'applescript', path: 'brace/mode/applescript' },
+  { name: 'forth', path: 'brace/mode/forth' },
+  { name: 'julia', path: 'brace/mode/julia' },
+  { name: 'pgsql', path: 'brace/mode/pgsql' },
+  { name: 'sqlserver', path: 'brace/mode/sqlserver' },
+  { name: 'asciidoc', path: 'brace/mode/asciidoc' },
+  { name: 'fortran', path: 'brace/mode/fortran' },
+  { name: 'kotlin', path: 'brace/mode/kotlin' },
+  { name: 'php', path: 'brace/mode/php' },
+  { name: 'stylus', path: 'brace/mode/stylus' },
+  { name: 'assembly_x86', path: 'brace/mode/assembly_x86' },
+  { name: 'ftl', path: 'brace/mode/ftl' },
+  { name: 'latex', path: 'brace/mode/latex' },
+  { name: 'pig', path: 'brace/mode/pig' },
+  { name: 'svg', path: 'brace/mode/svg' },
+  { name: 'autohotkey', path: 'brace/mode/autohotkey' },
+  { name: 'gcode', path: 'brace/mode/gcode' },
+  { name: 'lean', path: 'brace/mode/lean' },
+  { name: 'plain_text', path: 'brace/mode/plain_text' },
+  { name: 'swift', path: 'brace/mode/swift' },
+  { name: 'batchfile', path: 'brace/mode/batchfile' },
+  { name: 'gherkin', path: 'brace/mode/gherkin' },
+  { name: 'less', path: 'brace/mode/less' },
+  { name: 'powershell', path: 'brace/mode/powershell' },
+  { name: 'swig', path: 'brace/mode/swig' },
+  { name: 'bro', path: 'brace/mode/bro' },
+  { name: 'gitignore', path: 'brace/mode/gitignore' },
+  { name: 'liquid', path: 'brace/mode/liquid' },
+  { name: 'praat', path: 'brace/mode/praat' },
+  { name: 'tcl', path: 'brace/mode/tcl' },
+  { name: 'c9search', path: 'brace/mode/c9search' },
+  { name: 'glsl', path: 'brace/mode/glsl' },
+  { name: 'lisp', path: 'brace/mode/lisp' },
+  { name: 'prolog', path: 'brace/mode/prolog' },
+  { name: 'tex', path: 'brace/mode/tex' },
+  { name: 'c_cpp', path: 'brace/mode/c_cpp' },
+  { name: 'gobstones', path: 'brace/mode/gobstones' },
+  { name: 'live_script', path: 'brace/mode/live_script' },
+  { name: 'properties', path: 'brace/mode/properties' },
+  { name: 'text', path: 'brace/mode/text' },
+  { name: 'cirru', path: 'brace/mode/cirru' },
+  { name: 'golang', path: 'brace/mode/golang' },
+  { name: 'livescript', path: 'brace/mode/livescript' },
+  { name: 'protobuf', path: 'brace/mode/protobuf' },
+  { name: 'textile', path: 'brace/mode/textile' },
+  { name: 'clojure', path: 'brace/mode/clojure' },
+  { name: 'graphqlschema', path: 'brace/mode/graphqlschema' },
+  { name: 'logiql', path: 'brace/mode/logiql' },
+  { name: 'python', path: 'brace/mode/python' },
+  { name: 'toml', path: 'brace/mode/toml' },
+  { name: 'cobol', path: 'brace/mode/cobol' },
+  { name: 'groovy', path: 'brace/mode/groovy' },
+  { name: 'lsl', path: 'brace/mode/lsl' },
+  { name: 'r', path: 'brace/mode/r' },
+  { name: 'tsx', path: 'brace/mode/tsx' },
+  { name: 'haml', path: 'brace/mode/haml' },
+  { name: 'lua', path: 'brace/mode/lua' },
+  { name: 'razor', path: 'brace/mode/razor' },
+  { name: 'turtle', path: 'brace/mode/turtle' },
+  { name: 'coldfusion', path: 'brace/mode/coldfusion' },
+  { name: 'handlebars', path: 'brace/mode/handlebars' },
+  { name: 'luapage', path: 'brace/mode/luapage' },
+  { name: 'rdoc', path: 'brace/mode/rdoc' },
+  { name: 'twig', path: 'brace/mode/twig' },
+  { name: 'csharp', path: 'brace/mode/csharp' },
+  { name: 'haskell', path: 'brace/mode/haskell' },
+  { name: 'lucene', path: 'brace/mode/lucene' },
+  { name: 'red', path: 'brace/mode/red' },
+  { name: 'typescript', path: 'brace/mode/typescript' },
+  { name: 'csound_document', path: 'brace/mode/csound_document' },
+  { name: 'haskell_cabal', path: 'brace/mode/haskell_cabal' },
+  { name: 'makefile', path: 'brace/mode/makefile' },
+  { name: 'rhtml', path: 'brace/mode/rhtml' },
+  { name: 'vala', path: 'brace/mode/vala' },
+  { name: 'csound_orchestra', path: 'brace/mode/csound_orchestra' },
+  { name: 'haxe', path: 'brace/mode/haxe' },
+  { name: 'markdown', path: 'brace/mode/markdown' },
+  { name: 'rst', path: 'brace/mode/rst' },
+  { name: 'vbscript', path: 'brace/mode/vbscript' },
+  { name: 'csound_score', path: 'brace/mode/csound_score' },
+  { name: 'hjson', path: 'brace/mode/hjson' },
+  { name: 'mask', path: 'brace/mode/mask' },
+  { name: 'ruby', path: 'brace/mode/ruby' },
+  { name: 'velocity', path: 'brace/mode/velocity' },
+  { name: 'css', path: 'brace/mode/css' },
+  { name: 'html', path: 'brace/mode/html' },
+  { name: 'matlab', path: 'brace/mode/matlab' },
+  { name: 'rust', path: 'brace/mode/rust' },
+  { name: 'verilog', path: 'brace/mode/verilog' },
+  { name: 'curly', path: 'brace/mode/curly' },
+  { name: 'html_elixir', path: 'brace/mode/html_elixir' },
+  { name: 'mavens_mate_log', path: 'brace/mode/mavens_mate_log' },
+  { name: 'sass', path: 'brace/mode/sass' },
+  { name: 'vhdl', path: 'brace/mode/vhdl' },
+  { name: 'd', path: 'brace/mode/d' },
+  { name: 'html_ruby', path: 'brace/mode/html_ruby' },
+  { name: 'maze', path: 'brace/mode/maze' },
+  { name: 'scad', path: 'brace/mode/scad' },
+  { name: 'wollok', path: 'brace/mode/wollok' },
+  { name: 'dart', path: 'brace/mode/dart' },
+  { name: 'ini', path: 'brace/mode/ini' },
+  { name: 'mel', path: 'brace/mode/mel' },
+  { name: 'scala', path: 'brace/mode/scala' },
+  { name: 'xml', path: 'brace/mode/xml' },
+  { name: 'diff', path: 'brace/mode/diff' },
+  { name: 'io', path: 'brace/mode/io' },
+  { name: 'mips_assembler', path: 'brace/mode/mips_assembler' },
+  { name: 'scheme', path: 'brace/mode/scheme' },
+  { name: 'xquery', path: 'brace/mode/xquery' },
+  { name: 'django', path: 'brace/mode/django' },
+  { name: 'jack', path: 'brace/mode/jack' },
+  { name: 'mipsassembler', path: 'brace/mode/mipsassembler' },
+  { name: 'scss', path: 'brace/mode/scss' },
+  { name: 'yaml', path: 'brace/mode/yaml' },
+  { name: 'dockerfile', path: 'brace/mode/dockerfile' },
+  { name: 'jade', path: 'brace/mode/jade' },
+  { name: 'mushcode', path: 'brace/mode/mushcode' },
+  { name: 'sh', path: 'brace/mode/sh' },
+  { name: 'dot', path: 'brace/mode/dot' },
+  { name: 'java', path: 'brace/mode/java' },
+  { name: 'mysql', path: 'brace/mode/mysql' },
+  { name: 'sjs', path: 'brace/mode/sjs' },
+  { name: 'drools', path: 'brace/mode/drools' },
+  { name: 'javascript', path: 'brace/mode/javascript' },
+  { name: 'nix', path: 'brace/mode/nix' },
+  { name: 'smarty', path: 'brace/mode/smarty' }
+]
+
+export {
+  modelist
+}
---
+++ src/lib/themes.ts
@@ -0,0 +1,82 @@
+
+export * from 'brace/theme/ambiance'
+export * from 'brace/theme/eclipse'
+export * from 'brace/theme/merbivore_soft'
+export * from 'brace/theme/tomorrow_night'
+export * from 'brace/theme/chaos'
+export * from 'brace/theme/github'
+export * from 'brace/theme/mono_industrial'
+export * from 'brace/theme/tomorrow_night_blue'
+export * from 'brace/theme/chrome'
+export * from 'brace/theme/gob'
+export * from 'brace/theme/monokai'
+export * from 'brace/theme/tomorrow_night_bright'
+export * from 'brace/theme/clouds'
+export * from 'brace/theme/gruvbox'
+export * from 'brace/theme/pastel_on_dark'
+export * from 'brace/theme/tomorrow_night_eighties'
+export * from 'brace/theme/clouds_midnight'
+export * from 'brace/theme/idle_fingers'
+export * from 'brace/theme/solarized_dark'
+export * from 'brace/theme/twilight'
+export * from 'brace/theme/cobalt'
+export * from 'brace/theme/iplastic'
+export * from 'brace/theme/solarized_light'
+export * from 'brace/theme/vibrant_ink'
+export * from 'brace/theme/crimson_editor'
+export * from 'brace/theme/katzenmilch'
+export * from 'brace/theme/sqlserver'
+export * from 'brace/theme/xcode'
+export * from 'brace/theme/dawn'
+export * from 'brace/theme/kr_theme'
+export * from 'brace/theme/terminal'
+export * from 'brace/theme/dracula'
+export * from 'brace/theme/kuroir'
+export * from 'brace/theme/textmate'
+export * from 'brace/theme/dreamweaver'
+export * from 'brace/theme/merbivore'
+export * from 'brace/theme/tomorrow'
+
+const themelist = [
+  { name: 'ambiance', path: 'brace/theme/ambiance' },
+  { name: 'eclipse', path: 'brace/theme/eclipse' },
+  { name: 'merbivore_soft', path: 'brace/theme/merbivore_soft' },
+  { name: 'tomorrow_night', path: 'brace/theme/tomorrow_night' },
+  { name: 'chaos', path: 'brace/theme/chaos' },
+  { name: 'github', path: 'brace/theme/github' },
+  { name: 'mono_industrial', path: 'brace/theme/mono_industrial' },
+  { name: 'tomorrow_night_blue', path: 'brace/theme/tomorrow_night_blue' },
+  { name: 'chrome', path: 'brace/theme/chrome' },
+  { name: 'gob', path: 'brace/theme/gob' },
+  { name: 'monokai', path: 'brace/theme/monokai' },
+  { name: 'tomorrow_night_bright', path: 'brace/theme/tomorrow_night_bright' },
+  { name: 'clouds', path: 'brace/theme/clouds' },
+  { name: 'gruvbox', path: 'brace/theme/gruvbox' },
+  { name: 'pastel_on_dark', path: 'brace/theme/pastel_on_dark' },
+  { name: 'tomorrow_night_eighties', path: 'brace/theme/tomorrow_night_eighties' },
+  { name: 'clouds_midnight', path: 'brace/theme/clouds_midnight' },
+  { name: 'idle_fingers', path: 'brace/theme/idle_fingers' },
+  { name: 'solarized_dark', path: 'brace/theme/solarized_dark' },
+  { name: 'twilight', path: 'brace/theme/twilight' },
+  { name: 'cobalt', path: 'brace/theme/cobalt' },
+  { name: 'iplastic', path: 'brace/theme/iplastic' },
+  { name: 'solarized_light', path: 'brace/theme/solarized_light' },
+  { name: 'vibrant_ink', path: 'brace/theme/vibrant_ink' },
+  { name: 'crimson_editor', path: 'brace/theme/crimson_editor' },
+  { name: 'katzenmilch', path: 'brace/theme/katzenmilch' },
+  { name: 'sqlserver', path: 'brace/theme/sqlserver' },
+  { name: 'xcode', path: 'brace/theme/xcode' },
+  { name: 'dawn', path: 'brace/theme/dawn' },
+  { name: 'kr_theme', path: 'brace/theme/kr_theme' },
+  { name: 'terminal', path: 'brace/theme/terminal' },
+  { name: 'dracula', path: 'brace/theme/dracula' },
+  { name: 'kuroir', path: 'brace/theme/kuroir' },
+  { name: 'textmate', path: 'brace/theme/textmate' },
+  { name: 'dreamweaver', path: 'brace/theme/dreamweaver' },
+  { name: 'merbivore', path: 'brace/theme/merbivore' },
+  { name: 'tomorrow', path: 'brace/theme/tomorrow}' }
+]
+
+export {
+  themelist
+}
