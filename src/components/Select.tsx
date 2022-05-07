8702dd3c6c552f31522f097d648a5c8715d98f5b
Alice Huang <alicehccn@gmail.com>
2022-05-06 20:12:58 -0700

    0.1.1-dev (#14) (#15)
    * import modes and themes in separate files
    
    * prototype theme selection
    
    * bootstrap mui
    
    * can set theme now
    
    * restructure folder
    
    * rename some stuff
    
    * Canvas redesign (#13)
    
    * simplied canvas design
    
    * modulizing cpm
    
    * same as above
    
    * refactor constainers/components
    
    * clean up
    
    * merge changes to root
    
    * linting
    
    * update version

---
+++ src/components/Select.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import Button from '@mui/material/Button'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+
+import './Select.scss'
+
+interface OptionType {
+  [key: string]: string
+}
+
+interface PropType {
+  name: string
+  options: OptionType[]
+  selectOption: Function
+}
+
+export default function Select (props: PropType): JSX.Element {
+  const { options, name } = props
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const [selected, selectOption] = useState(name)
+  const open = Boolean(anchorEl)
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    setAnchorEl(e.currentTarget)
+  }
+  const handleClose = (e: React.MouseEvent<HTMLElement>): void => {
+    setAnchorEl(null)
+    const option: string = e.currentTarget.id
+    props.selectOption(option)
+    selectOption(option)
+  }
+
+  return (
+    <div className='select'>
+      <Button
+        id='basic-button'
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup='true'
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        className='toolbar-item'
+      >
+        {selected}
+      </Button>
+      <Menu
+        id='basic-menu'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button'
+        }}
+      >
+        {options.map((option: OptionType) =>
+          <MenuItem
+            key={option.path}
+            onClick={handleClose}
+            id={option.name}
+          >
+            {option.name}
+          </MenuItem>
+        )}
+      </Menu>
+    </div>
+  )
+}
---
+++ src/containers/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import Canvas from './Canvas'
+// import Editor from './Editor'
+
+export default function App (): JSX.Element {
+  return (
+    <div className='App'>
+      {/* <Editor /> */}
+      <Canvas />
+    </div>
+  )
+}
